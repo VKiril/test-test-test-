470385ffb75ff1654cc895ab58778d6e98efda4c
VKiril <everest0corp@gmail.com>
2016-06-28 18:12:45 +0300

    test

--- src/WorkActivityBundle/Resources/public/js/main.js
+++ src/WorkActivityBundle/Resources/public/js/main.js
@@ -22,4 +22,21 @@ $(document).ready(function () {
 
         $(".form-horizontal .control-group:last").remove();
     });
+
+    $( "#from" ).datepicker({
+        defaultDate: "+1w",
+        changeMonth: true,
+        numberOfMonths: 3,
+        onClose: function( selectedDate ) {
+            $( "#to" ).datepicker( "option", "minDate", selectedDate );
+        }
+    });
+    $( "#to" ).datepicker({
+        defaultDate: "+1w",
+        changeMonth: true,
+        numberOfMonths: 3,
+        onClose: function( selectedDate ) {
+            $( "#from" ).datepicker( "option", "maxDate", selectedDate );
+        }
+    });
 });
